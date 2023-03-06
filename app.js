11708a54b230217801ab6e5d3dcba6b6d7417e02
Immortal <109055473+ImmortalRio27@users.noreply.github.com>
2023-03-07 00:15:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var arr = [];
+     // создается пустой массив, внутрь которого будут помещаться имена 
+
+for (var stopCycle = 'stop'; command != stopCycle;) { // создается бесконечный цикл, который можно прервать командой stop
+    var userName = prompt('Имя');  
+                             //поле ввода пользователя
+    var command = prompt('add / del / stop ?'); 
+     // создается переменная с командами (add (добавляем элемент в массив) / del(удаление элемента из массива) / stop(остановка цикла))
+
+    if(command == 'add'){ 
+                     //проверяет на команду add(добавляем элемент)
+        arr.push(userName)  
+                   //добавляем элемент в массив
+    }else if(command == 'del') {
+               //проверяет на команду del(удаляем элемент)
+        let deleteCommand = arr.indexOf(userName)
+            //получаем позицию справа
+        let deleteCommand2 = arr.lastIndexOf(userName)
+         //получаем позицию слева 
+        arr.splice(deleteCommand, deleteCommand2)
+           //удаляем нужный нам элемент (начиная от deleteCommand, заканчивая deleteCommand2)
+    }
+    
+
+   
+}
+console.log(arr);
+  // после завершения цикла  (command == stop) выводим массив в консоль
